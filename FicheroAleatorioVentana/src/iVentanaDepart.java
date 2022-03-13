dc90f24dd04bbbe4fb874e7fcf920864e6450420
Diego Gutierrez <gutixtreme@gmail.com>
2022-03-13 21:02:40 +0100

    Entrega

---
+++ FicheroAleatorioVentana/src/iVentanaDepart.java
@@ -0,0 +1,13 @@
+import java.awt.event.ActionEvent;
+
+public interface iVentanaDepart {
+
+	void modificacion(String NOEXISTE_DEPART);
+
+	void borra(String NOEXISTE_DEPART);
+
+	void alta();
+
+	void consultas(ActionEvent e);
+
+}
